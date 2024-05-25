d88edefa14e841f26c03316fa039f437d7277366
Temo4kin <temo4kinn@gmail.com>
2024-05-25 17:24:51 +0300

    Обновление решения первой задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = (b * b) - (4 * a * c);
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+ if (d === 0) {
     arr.push((-b)/(2 * a));
   } else if (d > 0) {
     arr.push((-b + Math.sqrt(d) )/(2*a));
