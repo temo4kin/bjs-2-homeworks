402df318c4575452ff28566de7e5ceffdefeae87
Temo4kin <temo4kinn@gmail.com>
2024-06-05 22:21:25 +0300

    Homework 2 - Исправил первое и второе задания

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -20,56 +20,52 @@ function getArrayParams(...arr) {
 function summElementsWorker(...arr) {
   if (arr.length === 0) {
     return 0;
-  } else {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-      sum += arr[i];
-    }
-    return sum;
   }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
   if (arr.length === 0) {
     return 0;
-  } else {
-    let max = Math.max(...arr);
-    let min = Math.min(...arr);
-    return max - min;
   }
+  let max = Math.max(...arr);
+  let min = Math.min(...arr);
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
   if (arr.length === 0) {
     return 0;
-  } else {
-    let sumEvenElement = 0;
-    let sumOddElement = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 === 0) {
-        sumEvenElement += arr[i];
-      } else {
-        sumOddElement += arr[i];
-      }
+  } 
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
     }
-    return sumEvenElement - sumOddElement;
   }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
   if (arr.length === 0) {
     return 0;
-  } else {
-    let sumEvenElement = 0;
-    let countEvenElement = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 === 0) {
-        sumEvenElement += arr[i];
-        countEvenElement++;
-      }
-    }
-    return sumEvenElement / countEvenElement;
   }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+  return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
