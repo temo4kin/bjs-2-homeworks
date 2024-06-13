c82e39325d6610e1851b26f4d7a9fff1816e3ceb
Temo4kin <temo4kinn@gmail.com>
2024-06-13 21:16:45 +0300

    Homework 4 - Объекты - создал функции, но не смог исправить одну ошибку

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks === undefined) {
+    return 0;
+  } else {
+    this.marks = this.marks.concat(marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks === undefined) {
+    return 0;
+  } else {
+    return this.marks.reduce((a, b) => a + b) / this.marks.length;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
 }
